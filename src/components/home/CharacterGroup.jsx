73cdb17349a4d7d4474d41a3e93260896e23b12c
ketchup0211 <dlcka1111@naver.com>
2025-11-25 01:12:25 +0900

    [ADD] CharacterGroup todo

--- src/components/home/CharacterGroup.jsx
+++ src/components/home/CharacterGroup.jsx
@@ -1,10 +1,19 @@
 import styles from "./CharacterGroup.module.css";
 
+//members = [{name, progress, character_image_id, rank}]
 function CharacterGroup({}) {
   return (
     <div>
       <p>캐릭터 컨테이너</p>
-      <p>개별 캐릭터 상태, 캐릭터, 이름</p>
+      {/*member 정보에 대해 map*/}
+      <p>progress 말풍선</p>
+      <p>character_image_id</p>
+      {/*haracter_image_id로 이미지 가져오는 로직 구현 필요*/}
+      <span>
+        <p>rank</p>
+        {/*rank에 따라 이미지 가져오는 로직 구현 필요*/}
+        <p>name</p>
+      </span>
     </div>
   );
 }
