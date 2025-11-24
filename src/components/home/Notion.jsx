57c3581e4f3ceeb6d276c53e154531fb66c74bda
ketchup0211 <dlcka1111@naver.com>
2025-11-25 01:43:24 +0900

    [ADD] svg toggle-icon as react component

--- src/components/home/Notion.jsx
+++ src/components/home/Notion.jsx
@@ -1,5 +1,6 @@
 import styles from "./Notion.module.css";
 import NotionIcon from "../../assets/notion-icon.svg?react";
+import ToggleIcon from "../../assets/toggle-icon.svg?react";
 
 function Notion({ notion }) {
   return (
@@ -8,7 +9,7 @@ function Notion({ notion }) {
         <NotionIcon />
         <p className={styles.text}>{notion}</p>
       </div>
-      <p>토글 아이콘</p>
+      <ToggleIcon />
     </div>
   );
 }
