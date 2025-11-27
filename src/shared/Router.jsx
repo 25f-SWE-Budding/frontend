50d8cfc8d30ab755553e987e027c4dee5fa00776
ketchup0211 <dlcka1111@naver.com>
2025-11-27 22:45:12 +0900

    [ADD] Router add character page

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/home/Home";
 import Profile from "../pages/profile/Profile";
 import BottomNav from "../components/layout/BottomNav";
 import Header from "../components/layout/Header";
+import Character from "../pages/character/Character";
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/character" element={<Character />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
       <BottomNav />
