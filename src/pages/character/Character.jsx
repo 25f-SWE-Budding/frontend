e74c0ab93a635ff1f83eef71bdce5cb20c5348c3
ketchup0211 <dlcka1111@naver.com>
2025-11-27 23:21:36 +0900

    [ADD] top button

--- src/pages/character/Character.jsx
+++ src/pages/character/Character.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import styles from "./Character.module.css";
 import starIcon from "../../assets/star-icon.png";
+import Button from "../../components/ui/Button";
 
 function Character() {
   return (
@@ -8,9 +9,14 @@ function Character() {
       <div className="characterContainer" id={styles.background}>
         <div>
           <div>
-            <img src={starIcon} alt="star-icon" width="47" />
-            <p>나의 도전</p>
-            <p>공지</p>
+            <span>
+              <img src={starIcon} alt="star-icon" width="47" />
+              <Button name={"나의 도전"} url={"/"} />
+            </span>
+            <span>
+              <img src={starIcon} alt="star-icon" width="47" />
+              <Button name={"공지"} url={"/"} />
+            </span>
           </div>
           <p>point</p>
         </div>
