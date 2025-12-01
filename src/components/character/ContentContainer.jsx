922978832eb0fcd1baf00e98c9d6bbf1a6f29aff
ketchup0211 <dlcka1111@naver.com>
2025-12-02 04:53:51 +0900

    [ADD] Content Container title

--- src/components/character/ContentContainer.jsx
+++ src/components/character/ContentContainer.jsx
@@ -6,9 +6,15 @@ function ContentContainer({}) {
         <p>포인트상점</p>
       </div>
       <div className={styles.contentContainer}>
-        <p>bottom bar</p>
-        <p>bottom bar</p>
-        <p>bottom bar</p>
+        <div className={styles.contents}>
+          <div className={styles.contentTitle}>
+            <p className={styles.textEmphasize}>일일혜택</p>
+            <p className={styles.textBold}>매일매일 지급되는 포인트</p>
+          </div>
+          <p>bottom bar</p>
+          <p>bottom bar</p>
+          <p>bottom bar</p>
+        </div>
       </div>
     </div>
   );
