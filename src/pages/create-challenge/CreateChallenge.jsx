a53879e531dccc34f246685f1c3ed6af5eaff498
ketchup0211 <dlcka1111@naver.com>
2025-12-03 23:10:46 +0900

    [FIX] CSS overflow

--- src/pages/create-challenge/CreateChallenge.jsx
+++ src/pages/create-challenge/CreateChallenge.jsx
@@ -2,30 +2,25 @@ import { useState, useEffect } from "react";
 import styles from "./CreateChallenge.module.css";
 import NavigateBar from "../../components/ui/NavigateBar";
 import CategoryField from "../../components/create-challenge/CategoryField";
+import TitleField from "../../components/create-challenge/TitleField";
 
 function CreateChallenge() {
+  const [title, setTitle] = useState("");
   return (
     <div className={styles.createChallenge}>
       <NavigateBar title={"도전 생성하기"} disableCreateBtn={true} />
-      <div>
-        <p>무엇에 함께 도전할까요?</p>
-        <input
-          type="text"
-          placeholder="도전 이름을 입력해주세요 (최대 15자)"
-          maxLength={15}
-        />
-      </div>
-      <div>
+      <div className={styles.container}>
+        <TitleField value={title} onChange={(e) => setTitle(e.target.value)} />
         <CategoryField />
-      </div>
-      <div>
-        <p>함께할 친구</p>
-        <div></div>
-        <input
-          type="text"
-          placeholder="친구 이름을 검색하세요"
-          maxLength={15}
-        />
+        <div>
+          <p>함께할 친구</p>
+          <div></div>
+          <input
+            type="text"
+            placeholder="친구 이름을 검색하세요"
+            maxLength={15}
+          />
+        </div>
       </div>
     </div>
   );
