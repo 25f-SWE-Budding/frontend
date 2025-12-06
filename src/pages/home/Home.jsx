48aecc1fb68862b638a21fecd9ea882cbe97a79e
Ellsy23 <dlwnsgus3153@naver.com>
2025-12-06 20:47:13 +0900

    refactor: useFetch 훅 사용, 코드 구조 개선

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,34 +1,19 @@
-import { useState, useEffect } from "react";
-import styles from "./Home.module.css";
-
-import MissionHeader from "../../components/home/MissionHeader";
-import MemberInfo from "../../components/home/MemberInfo";
-import Notion from "../../components/home/Notion";
-import CharacterGroup from "../../components/home/CharacterGroup";
-import PhotoSection from "../../components/home/PhotoSection"; // [NEW] 추가
-import Calendar from "../../components/home/Calendar";
-import NavigateBar from "../../components/ui/NavigateBar";
+import styles from './Home.module.css';
+import MissionHeader from '../../components/home/MissionHeader';
+import MemberInfo from '../../components/home/MemberInfo';
+import Notion from '../../components/home/Notion';
+import CharacterGroup from '../../components/home/CharacterGroup';
+import PhotoSection from '../../components/home/PhotoSection';
+import Calendar from '../../components/home/Calendar';
+import NavigateBar from '../../components/ui/NavigateBar';
+import { useFetch } from '../../hooks/useFetch';
+import { MOCK_HOME_DATA } from '../../constants/mockData';
 
 function Home() {
-  const [data, setData] = useState({
-    succedDays: 0,
-    leftDays: 0,
-    goal: "",
-    prize: "",
-    members: [],
-    notion: "",
-  });
-
-  useEffect(() => {
-    // 실제 API 연동 전까지는 더미데이터가 화면에 보이도록,
-    // fetch 실패 시에도 기본값이 잘 렌더링되는지 확인해주세요.
-    fetch("http://localhost:5173/data/homeData.json")
-      .then((res) => res.json())
-      .then((data) => {
-        if (data && data[0]) setData(data[0]);
-      })
-      .catch((err) => console.log("데이터 로딩 실패(더미 사용 추천):", err));
-  }, []);
+  const { data } = useFetch(
+    'http://localhost:5173/data/homeData.json',
+    MOCK_HOME_DATA
+  );
 
   return (
     <div id={styles.container}>
@@ -40,19 +25,13 @@ function Home() {
         prize={data.prize}
       />
 
-      {/* 멤버 리스트 (가로 스크롤) */}
       <MemberInfo members={data.members} />
-
-      {/* 공지사항 */}
       <Notion notion={data.notion} />
-
-      {/* [1] 캐릭터 현황판 */}
       <CharacterGroup />
 
-      {/* [2] 인증 사진첩 (흰색 배경 박스 안으로 넣으려면 구조 변경 필요) */}
       <div className={styles.whiteSection}>
         <PhotoSection />
-        <div className={styles.divider}></div> {/* 간격 띄우기 */}
+        <div className={styles.divider}></div>
         <Calendar />
       </div>
     </div>
