366538e6ad4d8e327b1a6af15b383425e7ca3642
Ellsy23 <dlwnsgus3153@naver.com>
2025-12-06 20:43:42 +0900

    refactor: PropTypes 추가, React.memo 적용

--- src/components/home/MissionHeader.jsx
+++ src/components/home/MissionHeader.jsx
@@ -1,11 +1,13 @@
-import styles from "./MissionHeader.module.css";
+import { memo } from 'react';
+import PropTypes from 'prop-types';
+import styles from './MissionHeader.module.css';
 
-function MissionHeader({ succedDays, leftDays, goal, prize }) {
+const MissionHeader = memo(function MissionHeader({ succedDays, leftDays, goal, prize }) {
   return (
     <div className={styles.container}>
       <div className={styles.missionContainer}>
         <p className={styles.daysContainer}>
-          <span className={styles.days}>{succedDays}</span> 일째 도전 중이에요!{" "}
+          <span className={styles.days}>{succedDays}</span> 일껋 도전 중이에요!{' '}
           <span className={styles.days}>{leftDays}</span>일 남았어요
         </p>
         <p className={styles.goal}>{goal}</p>
@@ -14,16 +16,28 @@ function MissionHeader({ succedDays, leftDays, goal, prize }) {
         <div className={styles.imageFrame}>
           <img
             className={styles.image}
-            src={
-              "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQiOPMOqwlBx76tteUUGV0y73iTJmAbq7nCm6qiGq2cjJgii5lYneGIsmwuRWvw9BgwuKebg84C0qFjlliSnzSvFEJhZewpnO6e3_nukjZmejMshpObog66"
-            }
-            alt={"prize-" + prize}
+            src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQiOPMOqwlBx76tteUUGV0y73iTJmAbq7nCm6qiGq2cjJgii5lYneGIsmwuRWvw9BgwuKebg84C0qFjlliSnzSvFEJhZewpnO6e3_nukjZmejMshpObog66"
+            alt={prize ? `보상: ${prize}` : '보상 이미지'}
           />
         </div>
         <p className={styles.prize}>{prize}</p>
       </div>
     </div>
   );
-}
+});
+
+MissionHeader.propTypes = {
+  succedDays: PropTypes.number,
+  leftDays: PropTypes.number,
+  goal: PropTypes.string,
+  prize: PropTypes.string,
+};
+
+MissionHeader.defaultProps = {
+  succedDays: 0,
+  leftDays: 0,
+  goal: '',
+  prize: '',
+};
 
 export default MissionHeader;
