f7057c9603629a947a7398265a8461c46ab4186f
Ellsy23 <dlwnsgus3153@naver.com>
2025-12-06 20:41:21 +0900

    feat: 챌린지 목록, 홈 데이터 등 목 데이터

---
+++ src/constants/mockData.js
@@ -0,0 +1,41 @@
+export const MOCK_CHALLENGE_LIST = [
+  {
+    id: 1,
+    title: '주 3회 헬스장 가기',
+    subtitle: '건강한 몸 만들기 프로젝트',
+    type: 'single',
+  },
+  {
+    id: 2,
+    title: '아침 7시 기상하기',
+    subtitle: '미라클 모닝 챌린지',
+    type: 'group',
+  },
+  {
+    id: 3,
+    title: '하루 물 2L 마시기',
+    subtitle: '피부 좋아지는 습관',
+    type: 'single',
+  },
+  {
+    id: 4,
+    title: '매일 영단어 10개',
+    subtitle: '토익 만점 가자',
+    type: 'group',
+  },
+  {
+    id: 5,
+    title: '독서 30분',
+    subtitle: '한 달에 한 권 읽기',
+    type: 'group',
+  },
+];
+
+export const MOCK_HOME_DATA = {
+  succedDays: 0,
+  leftDays: 0,
+  goal: '',
+  prize: '',
+  members: [],
+  notion: '',
+};
